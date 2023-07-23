e57850ab56ce593d413f47ed66e8f2c3e672d6ef
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-07-23 13:50:26 +0300

    del code

--- TS-1/app.js
+++
@@ -1,9 +0,0 @@
-var btn = document.querySelector("button");
-var input1 = document.getElementById("1");
-var input2 = document.getElementById("2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-btn.addEventListener("click", function () {
-    return console.log(add(+input1.value, +input2.value));
-});
